77a239feda9b03087ffd8ee1b57fd2881b1eec4c
pegorino <eshkryabin@yandex.ru>
2019-02-04 00:37:43 +0300

    create forms

---
+++ persons/personsapp/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from personsapp.models import Person, Document
+
+
+class PersonModelForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super(PersonModelForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+class DocumentModelForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super(DocumentModelForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
