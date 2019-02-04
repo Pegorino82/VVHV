c700fb264994bdf4af50dd934bef4422d3977084
pegorino <eshkryabin@yandex.ru>
2019-02-04 18:53:59 +0300

    add update view, finish views

--- persons/personsapp/urls.py
+++ persons/personsapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from personsapp.views import PersonCreateView, PersonListView, PersonDetailView
+from personsapp.views import PersonCreateView, PersonListView, PersonDetailView, PersonUpdateView
+# from personsapp.views import PersonListView
 
 app_name = 'personsapp'
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('', PersonListView.as_view(), name='list_view'),
     path('create', PersonCreateView.as_view(), name='create_view'),
     path('detail/<int:pk>', PersonDetailView.as_view(), name='detail_view'),
+    path('update/<int:pk>', PersonUpdateView.as_view(), name='update_view'),
 ]
--- persons/personsapp/views.py
+++ persons/personsapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django.urls import reverse_lazy
+from django.forms import inlineformset_factory
+from django.db import transaction
 
-from personsapp.models import Person
-from personsapp.forms import PersonModelForm
+from personsapp.models import Person, Document
+from personsapp.forms import PersonModelForm, DocumentModelForm
 
 
 class PersonCreateView(CreateView):
@@ -11,14 +13,75 @@ class PersonCreateView(CreateView):
     form_class = PersonModelForm
     template_name = 'personsapp/create.html'
     success_url = reverse_lazy('personsapp:list_view')
+    extra_context = {'title': 'Create'}
+
+    formset_model = Document
+    formset_form = DocumentModelForm
+
+    def get_context_data(self, **kwargs):
+        document_formset = inlineformset_factory(self.model, self.formset_model, self.formset_form)
+        context = super(PersonCreateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            formset = document_formset(self.request.POST)
+        else:
+            formset = document_formset()
+        context['formset'] = formset
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+
+        with transaction.atomic():
+            self.object = form.save()
+            if formset.is_valid():
+                formset.instance = self.object
+                formset.save()
+
+        return super(PersonCreateView, self).form_valid(form)
 
 
 class PersonListView(ListView):
     model = Person
     template_name = 'personsapp/index.html'
     paginate_by = 3
+    extra_context = {'title': 'List'}
 
 
 class PersonDetailView(DetailView):
     model = Person
     template_name = 'personsapp/detail.html'
+    extra_context = {'title': 'Detail'}
+
+
+class PersonUpdateView(UpdateView):
+    model = Person
+    template_name = 'personsapp/update.html'
+    form_class = PersonModelForm
+    success_url = reverse_lazy('personsapp:list_view')
+    extra_context = {'title': 'Update'}
+
+    formset_model = Document
+    formset_form = DocumentModelForm
+
+    def get_context_data(self, **kwargs):
+        document_formset = inlineformset_factory(self.model, self.formset_model, self.formset_form)
+        context = super(PersonUpdateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            formset = document_formset(self.request.POST, instance=self.object)
+        else:
+            formset = document_formset(instance=self.object)
+        context['formset'] = formset
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+
+        with transaction.atomic():
+            self.object = form.save()
+            if formset.is_valid():
+                formset.instance = self.object
+                formset.save()
+
+        return super(PersonUpdateView, self).form_valid(form)
